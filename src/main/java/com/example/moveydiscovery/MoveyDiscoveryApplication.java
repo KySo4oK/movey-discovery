171d93030116f0d290fb4328886756e2a4dbb769
KySo4oK <prostorostuk08@gmail.com>
2021-03-19 18:08:01 +0200

    init discovery service

---
+++ src/main/java/com/example/moveydiscovery/MoveyDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.moveydiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveyDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoveyDiscoveryApplication.class, args);
+    }
+
+}
