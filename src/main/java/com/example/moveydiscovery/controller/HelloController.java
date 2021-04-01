461a9c7003ba18444b3d038a52d257a344a54f7c
KySo4oK <prostorostuk08@gmail.com>
2021-04-01 19:46:51 +0300

    add one simple consumer and producer

--- src/main/java/com/example/moveydiscovery/controller/HelloController.java
+++ src/main/java/com/example/moveydiscovery/controller/HelloController.java
@@ -1,15 +1,24 @@
-package com.example.moveydiscovery;
+package com.example.moveydiscovery.controller;
 
+import com.example.moveydiscovery.service.SimpleKafkaConsumerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/notification")
+@RequiredArgsConstructor
 public class HelloController {
+    private final SimpleKafkaConsumerService consumerService;
 
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello! Notification microservice is on Kubernetes now!";
+    }
+
+    @GetMapping("/kafka")
+    public String getMessage() {
+        return consumerService.getMessages().toString();
     }
 }
---
+++ src/main/java/com/example/moveydiscovery/service/SimpleKafkaConsumerService.java
@@ -0,0 +1,19 @@
+package com.example.moveydiscovery.service;
+
+import lombok.Getter;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SimpleKafkaConsumerService {
+    @Getter
+    private List<String> messages = new ArrayList<>();
+
+    @KafkaListener(id = "Starship", topics = {"server.starship"}, containerFactory = "singleFactory")
+    public void consume(String message) {
+        messages.add(message);
+    }
+}
