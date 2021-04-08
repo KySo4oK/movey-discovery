302d011d614c58345afe262a8f8eb67dc72f38cb
ivanluchkin <id.luchkin@gmail.com>
2021-04-08 23:13:02 +0300

    rabbit

--- src/main/java/com/example/moveydiscovery/controller/HelloController.java
+++ src/main/java/com/example/moveydiscovery/controller/HelloController.java
@@ -1,24 +1,33 @@
 package com.example.moveydiscovery.controller;
 
-import com.example.moveydiscovery.service.SimpleKafkaConsumerService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/notification")
-@RequiredArgsConstructor
 public class HelloController {
-    private final SimpleKafkaConsumerService consumerService;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Value("${spring.rabbitmq.exchange}")
+    private String exchange;
+    @Value("${spring.rabbitmq.routingkey}")
+    private String routingkey;
 
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello! Notification microservice is on Kubernetes now!";
     }
 
-    @GetMapping("/kafka")
-    public String getMessage() {
-        return consumerService.getMessages().toString();
+    @GetMapping("/emit/{message}")
+    String emitToMovieQueue(@PathVariable String message) {
+        rabbitTemplate.convertAndSend(exchange, routingkey, message);
+        return "Message sent";
     }
 }
